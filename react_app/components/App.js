874f8bc2ddff17d80dbb2fcc92950b43d1777d9c
Augustus Chang <augustus.chang@rutgers.edu>
2017-09-13 18:37:43 -0400

    rearrange directory

---
+++ react_app/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ToolBar from './ToolBar';
+
+export default () => {
+  const mainApp = (
+    <div>
+      <ToolBar />
+      <p> Plyground will go here </p>
+    </div>
+  );
+  return mainApp;
+};
---
+++ react_app/components/ToolBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default () => {
+  const toolBar = (
+    <button> Create new </button>
+  );
+  return toolBar;
+};
