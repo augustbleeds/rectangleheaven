3d26401a316c9aae2c3da62ffb43517d4bec73d0
Augustus Chang <augustus.chang@rutgers.edu>
2017-09-14 18:36:54 -0400

    set up dummy data

--- react_app/containers/Playground.js
+++ react_app/containers/Playground.js
@@ -1,3 +1,18 @@
 import React from 'react';
 
-export default () => <p> This is the awesome Playground </p>;
+const currentLayoutState = [
+  { x: 100, y: 100, height: 50, width: 50 },
+];
+
+export default class Playground extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { rectangles: currentLayoutState };
+  }
+
+  render() {
+    return (
+      <p> Hello this is the playground </p>
+    );
+  }
+}
