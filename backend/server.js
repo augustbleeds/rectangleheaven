00c611a7e70938b49e9b81a922f602ff3a375124
Augustus Chang <augustus.chang@rutgers.edu>
2017-09-19 14:57:51 -0400

    fix port

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
+const port = process.env.PORT;
+
 app.use(express.static('build'));
 
-app.listen(3000, () => {
-  console.log('Server for React Todo App listening on port 3000!');
+app.listen(port, () => {
+  console.log(`Server for React Todo App listening on port ${port}`);
 });
